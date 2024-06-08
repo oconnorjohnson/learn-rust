eac0d20b199be5ae7048a30c84c0015f414a46d9
Daniel Johnson <dojohnson@ucdavis.edu>
2024-06-08 14:37:50 -0700

    added c_to_f() function in temp_converter

--- temp_converter/src/main.rs
+++ temp_converter/src/main.rs
@@ -1,6 +1,26 @@
 use std::io;
 
 fn main() {
+    f_to_c();
+    c_to_f();
+}
+
+fn c_to_f() {
+    println!("Input a number to convert from celsius to farenheit.");
+    let mut c_temp = String::new();
+    io::stdin()
+        .read_line(&mut c_temp)
+        .expect("Failed to read line");
+    let c_temp: f32 = c_temp
+        .trim()
+        .parse()
+        .expect("Input cannot be parsed to a number");
+    println!("Converting {c_temp} degrees celsius to farenheit.");
+    let f_temp: f32 = 32.0 + (c_temp / (5.0 / 9.0));
+    println!("In farenheit, the temperature is {f_temp} degrees.");
+}
+
+fn f_to_c() {
     println!("Input a number to convert from farenheit to celsius.");
     let mut f_temp = String::new();
     io::stdin()
