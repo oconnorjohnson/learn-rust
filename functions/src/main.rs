1059e2c938c0b9aaed30edba52faa8a1f19a5e71
Daniel Johnson <dojohnson@ucdavis.edu>
2024-06-07 19:51:27 -0700

    more on functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,8 +1,20 @@
 fn main() {
     println!("started main");
     another_function(-5, 23.43);
+    let y = {
+        let x = 3;
+        x + 5
+    };
+    println!("y is equal to {y}");
+    let z = a_third_function(69418);
+    println!("z is equal to {z}");
 }
 
 fn another_function(x: i8, y: f32) {
     println!("started another_function with arguments of x: {x} and y: {y}");
+}
+
+fn a_third_function(x: u32) -> u32 {
+    println!("started a_third_function with an argument of x: {x}");
+    x + 2
 }
