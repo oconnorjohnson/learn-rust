ad47962d51dbc8d67740026b38cb760b8d8fb5f0
Daniel Johnson <dojohnson@ucdavis.edu>
2024-06-08 17:33:20 -0700

    stopping for now

--- twelve_days/src/main.rs
+++ twelve_days/src/main.rs
@@ -22,10 +22,12 @@ fn main() {
         "Twelve drummers drumming,",
     ];
     for (index, element) in days.iter().enumerate() {
+        // right now each verse is getting every day
+        // we want each verse (each run of the loop) to get a single on_the, day, and of_the
         curr_verse.push_str(&on_the);
         let curr_day = days[index];
         curr_verse.push_str(&curr_day);
-        println!("{curr_verse}");
+
         println!("{element}");
         // we need to loop through the days array, printing the day between the on_the and day_of
         // on each iteration if index > 0, we add all previous indices of 'presents' after the current is printed
@@ -37,3 +39,19 @@ fn main() {
     }
     println!("{lyrics}");
 }
+
+// current output:
+// Running `target/debug/twelve_days`
+// first
+// second
+// third
+// fourth
+// fifth
+// sixth
+// seventh
+// eighth
+// ninth
+// tenth
+// eleventh
+// twelve
+// On thefirstOn thefirstOn thesecondOn thefirstOn thesecondOn thethirdOn thefirstOn thesecondOn thethirdOn thefourthOn thefirstOn thesecondOn thethirdOn thefourthOn thefifthOn thefirstOn thesecondOn thethirdOn thefourthOn thefifthOn thesixthOn thefirstOn thesecondOn thethirdOn thefourthOn thefifthOn thesixthOn theseventhOn thefirstOn thesecondOn thethirdOn thefourthOn thefifthOn thesixthOn theseventhOn theeighthOn thefirstOn thesecondOn thethirdOn thefourthOn thefifthOn thesixthOn theseventhOn theeighthOn theninthOn thefirstOn thesecondOn thethirdOn thefourthOn thefifthOn thesixthOn theseventhOn theeighthOn theninthOn thetenthOn thefirstOn thesecondOn thethirdOn thefourthOn thefifthOn thesixthOn theseventhOn theeighthOn theninthOn thetenthOn theeleventhOn thefirstOn thesecondOn thethirdOn thefourthOn thefifthOn thesixthOn theseventhOn theeighthOn theninthOn thetenthOn theeleventhOn thetwelve
