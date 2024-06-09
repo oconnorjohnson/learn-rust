b39d505adc6a87b50a8749d111702a595519a6b4
Daniel Johnson <dojohnson@ucdavis.edu>
2024-06-08 17:46:49 -0700

    cleared curr_verse at beginning of each loop iteration

--- twelve_days/src/main.rs
+++ twelve_days/src/main.rs
@@ -1,25 +1,25 @@
 fn main() {
     let mut lyrics = String::from("");
     let mut curr_verse = String::from("");
-    let on_the: &str = "On the";
-    let _day_of: &str = "day of Christmas, my true love gave to me:";
+    let on_the: &str = "On the ";
+    let day_of: &str = " day of Christmas, my true love gave to me: ";
     let days = [
         "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth",
         "tenth", "eleventh", "twelve",
     ];
     let _presents = [
-        "A partridge in a pair tree.",
-        "Two turle doves, and",
-        "Three French hens,",
-        "Four calling birds,",
-        "Five golden rings,",
-        "Six geese a-laying,",
-        "Seven swans a-swimming,",
-        "Eight maids a-milking,",
-        "Nine ladies dancing,",
-        "Ten lords a-leaping,",
-        "Eleven pipers piping,",
-        "Twelve drummers drumming,",
+        "a partridge in a pair tree.",
+        "Two turle doves, and ",
+        "Three French hens, ",
+        "Four calling birds, ",
+        "Five golden rings, ",
+        "Six geese a-laying, ",
+        "Seven swans a-swimming, ",
+        "Eight maids a-milking, ",
+        "Nine ladies dancing, ",
+        "Ten lords a-leaping, ",
+        "Eleven pipers piping, ",
+        "Twelve drummers drumming, ",
     ];
     // each loop is a verse, as we're looping over the days array containing first, second, third etc.
     // while each verse has exponentially more until all elements from the presents array, each verse
@@ -27,11 +27,12 @@ fn main() {
     for (index, element) in days.iter().enumerate() {
         // right now each verse is getting every day
         // we want each verse (each run of the loop) to get a single on_the, day, and of_the
+        // we need to clear the curr_verse at beginning of each iteration
+        curr_verse.clear();
         curr_verse.push_str(&on_the);
-        let curr_day = days[index];
+        let curr_day = element;
         curr_verse.push_str(&curr_day);
-
-        println!("{element}");
+        curr_verse.push_str(&day_of);
         // we need to loop through the days array, printing the day between the on_the and day_of
         // on each iteration if index > 0, we add all previous indices of 'presents' after the current is printed
         // added in reverse order. This loop adds lyrics to the end of a string element (&mut lyrics)
