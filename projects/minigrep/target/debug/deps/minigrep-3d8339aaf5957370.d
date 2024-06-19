68102fd7db77ac955f4b65a0e075c9ce3531b6b9
Daniel Johnson <dojohnson@ucdavis.edu>
2024-06-19 14:08:31 -0700

    started minigrep project

---
+++ projects/minigrep/target/debug/deps/minigrep-3d8339aaf5957370.d
@@ -0,0 +1,5 @@
+/Users/danieljohnson/Desktop/development/personal/learn-rust/projects/minigrep/target/debug/deps/minigrep-3d8339aaf5957370: src/main.rs
+
+/Users/danieljohnson/Desktop/development/personal/learn-rust/projects/minigrep/target/debug/deps/minigrep-3d8339aaf5957370.d: src/main.rs
+
+src/main.rs:
---
+++ projects/minigrep/target/debug/minigrep.d
@@ -0,0 +1,1 @@
+/Users/danieljohnson/Desktop/development/personal/learn-rust/projects/minigrep/target/debug/minigrep: /Users/danieljohnson/Desktop/development/personal/learn-rust/projects/minigrep/src/main.rs
